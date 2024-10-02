0d619e77cd2f3f60283de66883c91529080594d8
lesliesackey <lesliesackey@localhost>
2024-10-02 14:10:33 -0500

    Stacey and I made a new flag.

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -17,14 +17,13 @@ public class Flag {
 		StdDraw.filledRectangle(0.7, 0.7, 0.7, 0.1);
 		
 		Color darkGreen = new Color(0, 150, 0);
-		
 		StdDraw.setPenColor(darkGreen);
 		StdDraw.filledRectangle(0.3, 0.3, 0.7, 0.1);
 		
 		StdDraw.setPenColor(Color.BLACK);
 		StdDraw.filledEllipse(0.5, 0.5, 0.045, 0.12);
 		
-		StdDraw.setPenColor(Color.RED);
+		StdDraw.setPenColor(Color.WHITE);
 		StdDraw.filledEllipse(0.5, 0.5, 0.020, 0.12);
 
 		
