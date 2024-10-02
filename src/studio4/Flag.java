1816cf55961aac9eb16884cd50014fbfdf30c4f5
lesliesackey <lesliesackey@localhost>
2024-10-02 14:10:21 -0500

    Stacey and I made flag, yay.

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,34 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.35);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.1);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.7, 0.7, 0.7, 0.1);
+		
+		Color darkGreen = new Color(0, 150, 0);
+		
+		StdDraw.setPenColor(darkGreen);
+		StdDraw.filledRectangle(0.3, 0.3, 0.7, 0.1);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledEllipse(0.5, 0.5, 0.045, 0.12);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledEllipse(0.5, 0.5, 0.020, 0.12);
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,9 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+
+		
 		
 	}
 }
